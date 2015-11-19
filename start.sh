5d01b609c724b69f998a34c946634e77dc9e97cd
Mate GABRI <mate@gabri.hu>
2015-11-19 10:51:49 +0100

    install latest version listen on port 80

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-varnishd -f /etc/varnish/default.vcl -s malloc,${MEMORY}M -a 0.0.0.0:8080
+varnishd -f /etc/varnish/default.vcl -s malloc,${MEMORY}M -a 0.0.0.0:80
 sleep 1
 varnishncsa
