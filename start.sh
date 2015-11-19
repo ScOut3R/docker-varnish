62af84786058efbcd6f919c5cf93a373fa00f22c
Mate GABRI <mate@gabri.hu>
2015-11-19 10:56:08 +0100

    default memory, configurable backend and TTL

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-varnishd -f /etc/varnish/default.vcl -s malloc,${MEMORY}M -a 0.0.0.0:80
+varnishd -f /etc/varnish/default.vcl \
+         -s malloc,${MEMORY:-128}M \
+         -a 0.0.0.0:80 \
+         -b ${BACKEND} \
+         -t ${TTL:-120}
 sleep 1
 varnishncsa
