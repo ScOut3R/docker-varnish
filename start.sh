116aae5944bf15b1619e59a58debff41b33fc9c8
Mate GABRI <mate@gabri.hu>
2015-11-19 11:58:04 +0100

    varnishncsa log format changed

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-varnishd -f /etc/varnish/default.vcl \
-         -s malloc,${MEMORY:-128}M \
+varnishd -s malloc,${MEMORY:-128}M \
          -a 0.0.0.0:80 \
          -b ${BACKEND} \
          -t ${TTL:-120}
 sleep 1
-varnishncsa
+varnishncsa -F '%{X-Forwarded-For}i %l %u %t "%r" %s %b "%{Referer}i" "%{User-agent}i" "%{Varnish:hitmiss}x"'
